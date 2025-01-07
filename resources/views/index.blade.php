bbb6266ea9a36b7985cc7de5a50428c6bbe7bf47
petreb904 <82363199+petreb904@users.noreply.github.com>
2025-01-07 11:10:48 +0100

    Initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en-hu">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laracoding.com CSS example</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <h1>sdfgh</h1>
+    <section class="section">
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <div class="card">
+                    <div class="header">
+                        <h1>1</h1>
+                    </div>
+                    <div class="container">
+                        <p>April 30, 2024</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</body>
+</html>
